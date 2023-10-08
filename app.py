efb19db7b86ce10a86f187d9d13fb64faba6d16d
Jean Marc Nadeau Huamani <101827583+Jean-Marc-NH@users.noreply.github.com>
2023-10-08 11:29:36 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -7,3 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/main-page')
+def mainPage():
+    return render_template('main-page.html')
+
+@app.route('/home')
+def template():
+    return render_template('template.html')
